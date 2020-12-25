3df304eed9245f0b69f90a2e82e4336c99c91753
travmalay <75923326+travmalay@users.noreply.github.com>
2020-12-25 08:24:09 +0800

    Update cameron.php

--- cameron.php
+++ cameron.php
@@ -46,31 +46,7 @@
     <title>Travel Blog</title>
   </head>
   <body>
-   <!--messsenger plugin-->
-  <!-- Load Facebook SDK for JavaScript -->
-  <div id="fb-root"></div>
-      <script>
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v9.0'
-          });
-        };
-
-        (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
-
-      <!-- Your Chat Plugin code -->
-      <div class="fb-customerchat"
-        attribution=setup_tool
-        page_id="106509371318196"
-  theme_color="#0A7CFF">
-      </div>
+   
       <!--comment-->
   <div id="fb-root"></div>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v9.0&appId=785790285105320&autoLogAppEvents=1" nonce="De6IImIY"></script>
