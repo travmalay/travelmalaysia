b4cce3832bb4001abd2490517a1c5f2ea55f16a9
travmalay <75923326+travmalay@users.noreply.github.com>
2020-12-25 08:59:00 +0800

    Update author.php

--- author.php
+++ author.php
@@ -102,31 +102,7 @@ mysqli_close($conn);
     <!--facebook page-->
     <div id="fb-root"></div>
 <script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v9.0&appId=785790285105320&autoLogAppEvents=1" nonce="ujfShQyD"></script>
-   <!--messsenger plugin-->
-  <!-- Load Facebook SDK for JavaScript -->
-  <div id="fb-root"></div>
-      <script>
-        window.fbAsyncInit = function() {
-          FB.init({
-            xfbml            : true,
-            version          : 'v9.0'
-          });
-        };
-
-        (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));</script>
-
-      <!-- Your Chat Plugin code -->
-      <div class="fb-customerchat"
-        attribution=setup_tool
-        page_id="106509371318196"
-  theme_color="#0A7CFF">
-      </div>
+  
     <!--logo and name-->
       
     <div class="jumbotron jumbotron-fluid" id="logoname">
