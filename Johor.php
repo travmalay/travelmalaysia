efa36100d02d58a7eb9350cf0e39a84a8662e7cc
travmalay <75923326+travmalay@users.noreply.github.com>
2023-09-03 16:12:54 +0200

    Johor.php

--- Johor.php
+++ Johor.php
@@ -602,16 +602,7 @@
    <!-- Go to www.addthis.com/dashboard to customize your tools -->
    <div class="addthis_inline_share_toolbox_ssp4"></div>
 
-<div id="fb-root"></div>
-    <script>(function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {return;}
-    js = d.createElement(s); js.id = id;
-    js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
-    fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));</script>
-
-     
+  
   <div class="fb-comments" data-href="https://travelmalaysia-0c83d610e294.herokuapp.com/Johor.php" data-width="" data-numposts=""></div>
 </div>
    
